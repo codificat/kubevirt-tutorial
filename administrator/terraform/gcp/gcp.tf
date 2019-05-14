27b7d8366356c7434c22ccda8821cdee2d6bf6b8
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2019-05-14 15:52:36 +0200

    Added GCP terraform initial version

---
+++ administrator/terraform/gcp/gcp.tf
@@ -0,0 +1,40 @@
+provider "google" {
+    project = "${var.gcp_project}"
+    region = "${var.gcp_region}"
+    zone = "${var.gcp_zone}" 
+}
+
+resource "google_compute_network" "default" {
+    name = "${var.gcp_network_name}"
+}
+
+resource "google_compute_firewall" "default" {
+    name    = "${var.gcp_firewall_rule_name}"
+    description = "Firewall rules for Kubevirt lab"
+    network = "${google_compute_network.default.name}"
+
+    allow {
+        protocol = "icmp"
+    }
+
+    allow {
+      protocol = "tcp"
+      ports    = ["80", "443", "8443"]
+    }
+    
+    source_tags = ["${var.gcp_instance_tag}"]
+}
+
+module "gcp_kubevirt_lab" {
+	source = "github.com/lumapps/terraform-google-compute-instance"
+	
+	amount       		= "${var.gcp_instances}"
+	region       		= "${var.gcp_region}"
+	name_prefix  		= "${var.hostname_prefix}"
+	machine_type 		= "${var.gcp_instance_size}"
+	boot_disk_size_gb   = "${var.gcp_boot_image_size_gb}"
+	boot_disk_image   	= "${var.gcp_boot_image}"
+    data_disk_size_gb   = "30"
+    service_account     = "${var.gcp_sa}"
+    tag					= "${var.gcp_instance_tag}"
+}
